7d4d939cee8918128434ea4b26baaf96f6b4bde8
Aaron Hoffman <ajh269@uakron.edu>
2023-03-08 16:14:00 -0500

    Correct helper files

---
+++ Testing/thermotest/helpers.cpp
@@ -0,0 +1,35 @@
+#include "helpers.h"
+
+#include <cstring>
+#include <limits>
+
+//Sliiiiiiiiiiiiide to the left
+// <--<--<--
+// [1, 2, 3] -> [2, 3, 0]
+void SlideLeft(double* arr, unsigned int size)
+{
+  memmove(arr, arr + 1, sizeof(double) * size);
+  arr[size - 1] = 0.0;
+
+  return;
+}
+
+//Sliiiiiiiiiiiiide to the right
+// -->-->-->
+// [1, 2, 3] -> [0, 1, 2]
+void SlideRight(double* arr, unsigned int size)
+{
+  memmove(arr + 1, arr, sizeof(double) * size);
+  arr[0] = 0.0;
+
+  return;
+}
+
+// (...chris-cross...?)
+void Prepend(double val, double* arr, unsigned int size)
+{
+  SlideRight(arr, size);
+  arr[0] = val;
+
+  return;
+}
--- Testing/thermotest/helpers.h
+++ Testing/thermotest/helpers.h
@@ -1,5 +1,5 @@
-#ifndef INCLUDED_HELPERS
-#define INCLUDED_HELPERS
+#ifndef INCLUDED_ARRAY_HELPERS
+#define INCLUDED_ARRAY_HELPERS
 
 // Helper functions
 #define sgn(x) ((x) < 0 ? -1 : ((x) > 0 ? 1 : 0))
@@ -7,48 +7,17 @@
 //Sliiiiiiiiiiiiide to the left
 // <--<--<--
 // [1, 2, 3] -> [2, 3, 0]
-void SlideLeft(double* arr, unsigned int size)
-{
-  memmove(arr, arr + 1, sizeof(double) * size);
-  arr[size - 1] = 0.0;
-
-  return;
-}
+void SlideLeft(double* arr, unsigned int size);
 
 //Sliiiiiiiiiiiiide to the right
 // -->-->-->
 // [1, 2, 3] -> [0, 1, 2]
-void SlideRight(double* arr, unsigned int size)
-{
-  memmove(arr + 1, arr, sizeof(double) * size);
-  arr[0] = 0.0;
-
-  return;
-}
+void SlideRight(double* arr, unsigned int size);
 
 // (...chris-cross...?)
-void Prepend(double val, double* arr, unsigned int size)
-{
-  SlideRight(arr, size);
-  arr[0] = val;
-
-  return;
-}
+void Prepend(double val, double* arr, unsigned int size);
 
 // print the contents of a given array
-void printArray(double* arr, unsigned int size)
-{
-  Serial.print("[");
-  for(uint i = 0; i < size-1; ++i)
-  {
-    Serial.print(arr[i]);
-    Serial.print(", ");    
-  }
-  Serial.print(arr[size-1]);
+void printArray(double* arr, unsigned int size);
 
-  Serial.print("]\n");
-
-  return;
-}
-
-#endif //INCLUDED_HELPERS
+#endif //INCLUDED_ARRAY_HELPERS
