128de56e980142f086f0bc0ed8655631e2a44268
Aaron Hoffman <ajh269@uakron.edu>
2023-03-08 15:30:45 -0500

    Extract helper functions to helpers.h

--- firmwar3/Testing/thermotest/thermotest.ino
+++ firmwar3/Testing/thermotest/thermotest.ino
@@ -2,11 +2,10 @@
 // https://learn.adafruit.com/thermocouple/
 
 #include <SPI.h>
+#include "helpers.h"
 
 // MAX6675 Pins
-#define thermocoupleDO 12
 #define thermocoupleCS 10
-#define thermocoupleCLK 13
 
 // Heater Pins
 #define heaterRelay 9
@@ -18,56 +17,6 @@ const int SIZE = 12;
 
 //MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 SPISettings thermocouple(4300000, MSBFIRST, SPI_MODE0);
-
-// Helper functions
-#define sgn(x) ((x) < 0 ? -1 : ((x) > 0 ? 1 : 0))
-
-//Sliiiiiiiiiiiiide to the left
-// <--<--<--
-// [1, 2, 3] -> [2, 3, 0]
-void SlideLeft(double* arr, unsigned int size)
-{
-  memmove(arr, arr + 1, sizeof(double) * size);
-  arr[size - 1] = 0.0;
-
-  return;
-}
-
-//Sliiiiiiiiiiiiide to the right
-// -->-->-->
-// [1, 2, 3] -> [0, 1, 2]
-void SlideRight(double* arr, unsigned int size)
-{
-  memmove(arr + 1, arr, sizeof(double) * size);
-  arr[0] = 0.0;
-
-  return;
-}
-
-// (...chris-cross...?)
-void Prepend(double val, double* arr, unsigned int size)
-{
-  SlideRight(arr, size);
-  arr[0] = val;
-
-  return;
-}
-
-// print the contents of a given array
-void printArray(double* arr, unsigned int size)
-{
-  Serial.print("[");
-  for(uint i = 0; i < size-1; ++i)
-  {
-    Serial.print(arr[i]);
-    Serial.print(", ");    
-  }
-  Serial.print(arr[size-1]);
-
-  Serial.print("]\n");
-
-  return;
-}
 
 void setup() {
   Serial.begin(9600);
