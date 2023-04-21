d25a1c133ed69671c9e7cd5fce2b2e1ce7595922
Aaron Hoffman <ajh269@uakron.edu>
2023-04-21 16:34:15 -0400

    Receive incoming transmission as a json doc

--- Testing/transmissiontest/transmissiontest.ino
+++ Testing/transmissiontest/transmissiontest.ino
@@ -1,3 +1,5 @@
+#include <ArduinoJson.h>
+
 // Note that we can't use the internal LED outside of this example because it is linked to CLK
 #define LEDPIN 13
 unsigned long ledOnTime = 0;
@@ -9,6 +11,16 @@ void setup() {
   pinMode(LEDPIN, OUTPUT);
 }
 
+StaticJsonDocument<128> cmd;
+String commandString = "";
+
+// Commanded distance to travel
+float distance;
+// Commanded length of material to extrude
+float materialLength;
+// Robot Movement Speed
+float speed;
+
 void loop() {
   UpdateLED();
 
@@ -16,9 +28,30 @@ void loop() {
   if(Serial.available()) {
     digitalWrite(LEDPIN, HIGH);
     ledOnTime = millis();
-    char c = Serial.read();
-    Serial.print(c);
+    commandString = Serial.readStringUntil('\n');
+    Serial.print("New Command String: ");
+    Serial.println(commandString);
+
+    DeserializationError error = deserializeJson(cmd, commandString.c_str());
+    if(error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+    }
+    else
+    {
+      Serial.println("New Command Variables:");
+      float distance = cmd["distance"];
+      float materialLength = cmd["materialLength"];
+      float newSpeed = cmd["newSpeed"];
+
+      Serial.printf("%s: %f\n", "distance", distance);
+      Serial.printf("%s: %f\n", "materialLength", materialLength);
+      Serial.printf("%s: %f\n", "newSpeed", newSpeed);
+
+      Serial.println();
+    }
   }
+
 }
 
 void UpdateLED()
