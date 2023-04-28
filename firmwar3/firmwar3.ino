893ce27f4f6bc4c3e311e28db701545a5ddee61d
Aaron Hoffman <ajh269@uakron.edu>
2023-04-28 15:29:47 -0400

    Improve commandExtruder function
    - Added timing limitation (extruder will turn off when it thinks the instruction should be completed)
    - Added MATERIAL_PER_E to actually utilize the E instruction as intended

--- firmwar3/firmwar3.ino
+++ firmwar3/firmwar3.ino
@@ -22,6 +22,8 @@ const unsigned long ONTIME = 50;
 const unsigned int INPUT_RESOLUTION = 800;
 // amount of material extruded per revolution (mm)
 const float MATERIAL_PER_REV = 17.4625;
+// amount of material to be extruded per E value (mm)
+const float MATERIAL_PER_E = 32.32;
 
 // Parsed command
 StaticJsonDocument<128> cmd;
@@ -32,6 +34,10 @@ float movementDistance;
 float movementSpeed;
 
 float lastCommandedMaterialLength = 0.0;
+
+bool isPerformingExtrusion = false;
+unsigned long extrusionStartTime = 0.0;
+unsigned long extrusionDuration = 0.0;
 
 void setup() {
   // Initalize Serial Port for communication
@@ -46,14 +52,13 @@ void setup() {
   // Initalize Communication Status Led
   pinMode(LEDPIN, OUTPUT);
 
-  digitalWrite(ENABLE_PIN, HIGH);
-
   Serial.println("Initalization Complete");
 }
 
 void loop() {
 
   UpdateCommand();
+  UpdateExtrusionTime();
 }
 
 void UpdateCommand() {
@@ -72,63 +77,92 @@ void parseCommand(String& commandString) {
   if(error) {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.f_str());
+
+    return;
   }
-  else {
-    movementDistance = cmd["distance"];
-    float commandedMaterialLength = cmd["materialLength"];
-    
-    if(commandedMaterialLength != 0.0)
-    {
-      materialToExtrude = commandedMaterialLength - lastCommandedMaterialLength;
-      lastCommandedMaterialLength = commandedMaterialLength;
-    }
-    else
-    {
-      materialToExtrude = commandedMaterialLength;
-      lastCommandedMaterialLength = commandedMaterialLength;
-    }
 
-    float newSpeed = cmd["newSpeed"];
-    if(newSpeed != -1.0)
-    {
-      movementSpeed = newSpeed;
-    }
+  movementDistance = cmd["distance"];
+  float commandedMaterialLength = cmd["materialLength"];
 
-    commandExtruder();
+  float newSpeed = cmd["newSpeed"];
+  if(newSpeed != -1.0)
+  {
+    movementSpeed = newSpeed;
+  }
+
+  if(commandedMaterialLength != 0.0)
+  {
+    materialToExtrude = commandedMaterialLength - lastCommandedMaterialLength;
+    lastCommandedMaterialLength = commandedMaterialLength;
+
+    if(materialToExtrude != 0.0) {
+      commandExtruder();
+    }
+  }
+  else
+  {
+    materialToExtrude = commandedMaterialLength;
+    lastCommandedMaterialLength = commandedMaterialLength;
   }
 }
 
+// It's commanding the extruder... but BETTER!
 void commandExtruder() {
 
-  Serial.println(movementDistance);
-  Serial.println(movementSpeed);
-  Serial.println(materialToExtrude);
+  // The amount of material needed to be extruded for this instruction
+  float instructionMaterial = abs(materialToExtrude) * MATERIAL_PER_E;
+  // Number of revolutions required to extrude the material
+  float numberRevolutions = instructionMaterial / MATERIAL_PER_REV;
+  // The amount of time it will take the extruder/arm to complete the instruction
+  float instructionTime;
+  // The extrusion rate to print properly (in rev/s)
+  float revolutionsPerSecond;
 
-  float instructionTime = movementDistance / movementSpeed;
-  float numberRevolutions = materialToExtrude; //* MATERIAL_PER_REV;
-  float revolutionRate = numberRevolutions; /// instructionTime;
-
-  float analogFrequency = revolutionRate * INPUT_RESOLUTION;
-
-  // Clockwise
-  if(analogFrequency > 0.0) {
-    digitalWrite(DIRECTION, HIGH);
+  // Calculate based on the instruction type (moving or stationary)
+  if(movementDistance > 0.1) {
+    // the instruction is a moving instruction
+    instructionTime = movementDistance / movementSpeed;
+    revolutionsPerSecond = numberRevolutions / instructionTime;
+  } else {
+    // the instruction can happen at an arbitrary set speed
+    revolutionsPerSecond = 2.0;
+    instructionTime = numberRevolutions / revolutionsPerSecond;
   }
-  // Counterclockwise
-  else {
+
+  // Analog frequency to achieve the given rev/s
+  float analogFrequency = revolutionsPerSecond * INPUT_RESOLUTION;
+
+  Serial.println();
+  Serial.println(instructionMaterial);
+  Serial.println(numberRevolutions);
+  Serial.println(instructionTime);
+  Serial.println(revolutionsPerSecond);
+  Serial.println(analogFrequency);
+  Serial.println();
+
+  if(materialToExtrude > 0.0) {
+    // CW; push material
+    digitalWrite(DIRECTION, HIGH);
+  } else {
+    // CCW; pull material
     digitalWrite(DIRECTION, LOW);
   }
 
-  Serial.printf("Raw Analog Freq: %f\n", analogFrequency);
-
-  if(abs(analogFrequency) > 4000.0) {
-    digitalWrite(LEDPIN, HIGH);
-    analogFrequency = 0.0;
-  }
-  //analogFrequency = min(abs(analogFrequency), 4000.0);
-
-  Serial.printf("Analog Freq: %f\n", analogFrequency);
-
   analogWriteFrequency(PULSE, abs(analogFrequency));
   analogWrite(PULSE, 128);
+
+  digitalWrite(ENABLE_PIN, HIGH);
+
+  extrusionStartTime = millis();
+  extrusionDuration = instructionTime * 1000.0;
+  isPerformingExtrusion = true;
+}
+
+void UpdateExtrusionTime() {
+
+  if(isPerformingExtrusion && millis() > extrusionStartTime + extrusionDuration) {
+    digitalWrite(ENABLE_PIN, LOW);
+    Serial.println("Instruction done");
+    isPerformingExtrusion = false;
+  }
 }
