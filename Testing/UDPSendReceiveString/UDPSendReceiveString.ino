3bc119c45a0e2262f1125094734532a94665aae9
Aaron Hoffman <ajh269@uakron.edu>
2023-11-13 15:42:45 -0500

    Remove unused code from example

--- Testing/UDPSendReceiveString/UDPSendReceiveString.ino
+++ Testing/UDPSendReceiveString/UDPSendReceiveString.ino
@@ -20,9 +20,6 @@
 // The IP address will be dependent on your local network:
 uint8_t mac[6];
 IPAddress ip(10, 0, 0, 111);
-IPAddress myDns(192, 168, 1, 1);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 0, 0);
 
 unsigned int localPort = 8888;      // local port to listen on
 
@@ -37,13 +34,6 @@ EthernetUDP Udp;
 
 void setup() {
   pinMode(LEDPIN, OUTPUT);
-  // You can use Ethernet.init(pin) to configure the CS pin
-  //Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
 
   teensyMAC(mac);
   //Ethernet.MACAddress(mac);
@@ -125,7 +115,6 @@ void teensyMAC(uint8_t *mac) {
   static char teensyMac[23];
   
   #if defined(HW_OCOTP_MAC1) && defined(HW_OCOTP_MAC0)
-    Serial.println("using HW_OCOTP_MAC* - see https://forum.pjrc.com/threads/57595-Serial-amp-MAC-Address-Teensy-4-0");
     for(uint8_t by=0; by<2; by++) mac[by]=(HW_OCOTP_MAC1 >> ((1-by)*8)) & 0xFF;
     for(uint8_t by=0; by<4; by++) mac[by+2]=(HW_OCOTP_MAC0 >> ((3-by)*8)) & 0xFF;
 
@@ -140,9 +129,7 @@ void teensyMAC(uint8_t *mac) {
     uint32_t SN=0;
     __disable_irq();
     
-    #if defined(HAS_KINETIS_FLASH_FTFA) || defined(HAS_KINETIS_FLASH_FTFL)
-      Serial.println("using FTFL_FSTAT_FTFA - vis teensyID.h - see https://github.com/sstaub/TeensyID/blob/master/TeensyID.h");
-      
+    #if defined(HAS_KINETIS_FLASH_FTFA) || defined(HAS_KINETIS_FLASH_FTFL)      
       FTFL_FSTAT = FTFL_FSTAT_RDCOLERR | FTFL_FSTAT_ACCERR | FTFL_FSTAT_FPVIOL;
       FTFL_FCCOB0 = 0x41;
       FTFL_FCCOB1 = 15;
@@ -152,9 +139,7 @@ void teensyMAC(uint8_t *mac) {
 
       #define MAC_OK
       
-    #elif defined(HAS_KINETIS_FLASH_FTFE)
-      Serial.println("using FTFL_FSTAT_FTFE - vis teensyID.h - see https://github.com/sstaub/TeensyID/blob/master/TeensyID.h");
-      
+    #elif defined(HAS_KINETIS_FLASH_FTFE)      
       kinetis_hsrun_disable();
       FTFL_FSTAT = FTFL_FSTAT_RDCOLERR | FTFL_FSTAT_ACCERR | FTFL_FSTAT_FPVIOL;
       *(uint32_t *)&FTFL_FCCOB3 = 0x41070000;
@@ -175,50 +160,5 @@ void teensyMAC(uint8_t *mac) {
 
   #ifdef MAC_OK
     sprintf(teensyMac, "MAC: %02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    Serial.println(teensyMac);
-  #else
-    Serial.println("ERROR: could not get MAC");
   #endif
 }
-
-/*
-  Processing sketch to run with this example
- =====================================================
-
- // Processing UDP example to send and receive string data from Arduino
- // press any key to send the "Hello Arduino" message
-
-
- import hypermedia.net.*;
-
- UDP udp;  // define the UDP object
-
-
- void setup() {
- udp = new UDP( this, 6000 );  // create a new datagram connection on port 6000
- //udp.log( true ); 		// <-- printout the connection activity
- udp.listen( true );           // and wait for incoming message
- }
-
- void draw()
- {
- }
-
- void keyPressed() {
- String ip       = "192.168.1.177";	// the remote IP address
- int port        = 8888;		// the destination port
-
- udp.send("Hello World", ip, port );   // the message to send
-
- }
-
- void receive( byte[] data ) { 			// <-- default handler
- //void receive( byte[] data, String ip, int port ) {	// <-- extended handler
-
- for(int i=0; i < data.length; i++)
- print(char(data[i]));
- println();
- }
- */
-
-
