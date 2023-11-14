fde17dca50ae6eb48e4ba7c7349b6d7caeef3f98
Aaron Hoffman <ajh269@uakron.edu>
2023-11-14 16:56:57 -0500

    Create pinout constructors

--- firmwar3/Extruder.cpp
+++ firmwar3/Extruder.cpp
@@ -1,8 +1,8 @@
 #include "Extruder.hpp"
 
-Extruder::Extruder() {
-
-}
+// https://en.wikipedia.org/wiki/C++11#Object_construction_improvement
+Extruder::Extruder()
+: Extruder(3,4,5,6) {}
 
 Extruder::Extruder(uint8_t hlfb, uint8_t pulse, uint8_t direction, uint8_t enable)
 : HLFB(hlfb), PULSE(pulse), DIRECTION(direction), ENABLE(enable) {
