7c9b3c49fd201b1642f69f609c7034a0d6632e9d
firejack6 <111947743+firejack6@users.noreply.github.com>
2025-02-20 18:07:47 -0500

    Post-Post Processing script for removing broken calls

---
+++ RoboDK/Programs/post_process.py
@@ -0,0 +1,60 @@
+# This script removes or comments out all Python calls to aid troubleshooting
+
+# Inputs ------------------------------------------
+job_dir = "./Mount/"
+job_file = "M_PRINT.JBI"
+
+# mode = "D" #delete
+mode = "C" #comment
+
+# Outputs ------------------------------------------
+# Files starting with the mode prefix
+
+
+
+# Code ---------------------------------------------
+job_dir_loc = job_file.split('/')
+currentLineNumber = 0
+fileListStart = None
+outfile = ""
+fileList = []
+# Open main job file that calls the other jobs
+# Create list of files for other jobs
+# Change name to reflect mode
+with open(job_dir + job_file,'r') as job:
+    for line in job:
+        ln = line.strip()
+        if ln == "NOP":
+            fileListStart = currentLineNumber
+        elif fileListStart == None:
+            if "//NAME" in ln:
+                ln = "//NAME "+ mode + ln[7:len(ln)]
+            ## in the header blocks
+        elif ln != "END":
+            ## List of files to call
+            fileList.append(ln.replace("CALL JOB:","")+".JBI")
+        outfile += ln + "\r\n"
+        
+# Write new job file
+with open(job_dir + mode + job_file,'w') as job:
+    job.write(outfile)
+    
+# Go through other job files
+# Remove calls
+# Rename to reflect mode
+for fname in fileList:
+    out_job_file = ""
+    with open(job_dir + fname,"r") as job:
+        for line in job:
+            ln = line.strip()
+            if mode == "replace":
+                ln = ln.replace("CALL","'CALL")
+            elif mode == "D":
+                if "CALL JOB" in ln:
+                    continue
+            if "//NAME" in ln:
+                ln = "//NAME "+ mode + ln[7:len(ln)]
+            out_job_file += ln + "\r\n"
+    # Write the new job file
+    with open(job_dir + mode+fname,"w") as job:
+        job.write(out_job_file)
